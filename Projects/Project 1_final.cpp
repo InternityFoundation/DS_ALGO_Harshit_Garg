197d4df65491a303861b741aefc3fb147e0f757c
Harshit Garg <60506389+harshit-learner@users.noreply.github.com>
2020-06-19 11:02:18 +0530

    Update Project 1_final.cpp

--- Projects/Project 1_final.cpp
+++ Projects/Project 1_final.cpp
@@ -1,3 +1,6 @@
+//This project is based on the number system converter. 
+//The user is shown a list of conversions and the user is required to choose the type of conversion he wants to do.
+
 #include<iostream>
 #include<string.h>
 #include<math.h>
